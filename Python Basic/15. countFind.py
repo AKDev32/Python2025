11ef07a930c233a3372a48fe9a71d126bd43b23c
AKDev32 <amankumar005562@gmail.com>
2025-08-29 10:40:44 +0530

    Added modular prog

---
+++ Python Basic/15. countFind.py
@@ -0,0 +1,12 @@
+# .count(), .find()
+
+# string = 'hellofhfjsdhwohefsndsjdbnijwdajsdjsdbcnsdbcds'
+string = input("Please Type Something... ")
+
+# print (string.find('o'))  # 4
+# print (string.count('aj'))
+
+if string.count('_') > 0:
+    print ('Not good!')
+else:
+    print ('good')    
---
+++ Python Basic/16. modularprog.py
@@ -0,0 +1,8 @@
+import math
+import myModule
+
+print(math.pi)
+print(math.degrees(math.pi))
+print (math.radians(60))
+print (myModule.myFunc(6))
+print (myModule.anotherFunc(15))
