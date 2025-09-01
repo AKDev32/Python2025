4941c23d07d77131109384cf81be8b475201cdc6
AKDev32 <amankumar005562@gmail.com>
2025-09-01 17:51:01 +0530

    static & class method

---
+++ Python Intermediate/2. staticAndClassMethod.py
@@ -0,0 +1,25 @@
+# static and class Method
+
+class person(object):
+
+    population = 50
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def getPopulation(cls):
+        return cls.population
+
+    @staticmethod
+    def isAdult(age):
+        return age>= 18
+
+    def display(self):
+        print(self.name, 'is', self.age, 'years old')
+
+newPerson = person('aman', 21)   
+print(newPerson.display())      
+print(person.getPopulation())       
+        
