4a442c83852db1dfedf309c95c36d46aa8368ae9
AKDev32 <amankumar005562@gmail.com>
2025-09-03 15:03:04 +0530

    Added collections

--- Python Intermediate/6. collections.py
+++ Python Intermediate/6. collections.py
@@ -1,13 +1,35 @@
+import collections
 from collections import Counter
 
-c = Counter('gallad')
-print(c)
-c = Counter(['a', 'b', 'c', 'd'])
-print(c)
-c = Counter({'a': 1, 'b': 2})
-print(c)
-c = Counter(cats=4, dogs=7)
-print(c)
-print(c['cats'])
-print(c['man'])
-print(list(c.elements()))
+# c = Counter('gallad')
+# print(c)
+# c = Counter(['a', 'b', 'c', 'd'])
+# print(c)
+# c = Counter({'a': 1, 'b': 2})
+# print(c)
+# c = Counter(cats=4, dogs=7)
+# print(c)
+# print(c['cats'])
+# print(c['man'])
+# print(list(c.elements()))
+
+
+# c = Counter(a=4, b=2, c=0, d=-2)
+# d = ['a', 'b', 'b', 'c']
+
+# c.subtract(d)
+# print(c)
+# c.update(d)
+# print(c)
+# c.clear()
+# print(c)
+
+
+c = Counter(a=4, b=2, c=0, d=-2)
+d = Counter(['a', 'b', 'b', 'c'])
+
+print(c + d)
+print(c - d)
+print(c & d)
+print(c | d)
+
